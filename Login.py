34f5d08e2521f0d6509ba96711d0b6352b5c66b9
HitheshPT <84070633+HitheshPT@users.noreply.github.com>
2023-04-13 23:17:57 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -4,8 +4,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# create a Service object
+service = Service(ChromeDriverManager().install())
+
+# pass the Service object to the Chrome driver
+driver = webdriver.Chrome(service=service)
 class mouseHover:
     def mouse(self):
         driver.get('https://genesis.qa.lrescorp.com/')
