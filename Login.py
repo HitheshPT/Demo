2f9d537f0166554ce896b762bf94fbe7b2968add
HitheshPT <84070633+HitheshPT@users.noreply.github.com>
2023-04-17 15:40:38 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,17 +1,23 @@
+  GNU nano 4.8                                                                      Login.py
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+from selenium.webdriver import FirefoxOptions
+
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=opts)
+
 class mouseHover:
     def mouse(self):
         driver.get('https://demo.actitime.com/login.do')
 
 mH = mouseHover()
 mH.mouse()
+
