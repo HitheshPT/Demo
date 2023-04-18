3c6f83c84eddc17cef075f630c48bd3d0428bae1
HitheshPT <84070633+HitheshPT@users.noreply.github.com>
2023-04-18 20:41:15 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-from selenium.webdriver import FirefoxOptions
+driver_path = ChromeDriverManager().install()
 
-opts = FirefoxOptions()
-opts.add_argument("--headless")
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=opts)
+
+
+driver = webdriver.Chrome(executable_path=driver_path,  chrome_options=chrome_options)
 
 class mouseHover:
     def mouse(self):
