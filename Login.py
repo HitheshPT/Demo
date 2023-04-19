a4b8d329904ac359799e32294ced9df9ce34744c
HitheshPT <84070633+HitheshPT@users.noreply.github.com>
2023-04-19 12:31:27 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -4,26 +4,38 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
-driver_path = ChromeDriverManager().install()
+driver = webdriver.Chrome(ChromeDriverManager().install())
+class loginLRES:
+    def login(self):
+        driver.get('https://genesis.qa.lrescorp.com/')
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        driver.find_element(By.ID, 'UserName').send_keys('KatalonTestUser')
+        driver.find_element(By.ID, 'Password').send_keys('Welcome1!')
+        driver.find_element(by=By.XPATH, value='//button[normalize-space()="Login"]').click()
+        driver.implicitly_wait(5)
+        orders = driver.find_element(by=By.XPATH, value='//span[normalize-space()="Orders"]')
+        createNewOrder = driver.find_element(by=By.XPATH, value="//span[normalize-space()='Create New Order']")
+        driver.implicitly_wait(5)
 
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
+        #Mouse Over Orders
+        actions = ActionChains(driver)
+        driver.implicitly_wait(5)
+        actions.move_to_element(orders).move_to_element(createNewOrder).click(createNewOrder).perform()
+
+        #Select clients from the dropdown.
+        clientDropDown = driver.find_element(by=By.XPATH, value="//select[@id='OrderItemEdit_ClientID']")
+        clientSelect = Select(clientDropDown)
+        available_options = clientSelect.options
+        for x in available_options:
+            if x.text == "DMD_VSA_TC2 (ID #444796)":
+                clientSelect.select_by_visible_text("DMD_VSA_TC2 (ID #444796)")
+                print("Client is found")
+                break
+            else:
+                print("Client is not found")
 
 
-
-driver = webdriver.Chrome(executable_path=driver_path,  chrome_options=chrome_options)
-
-class mouseHover:
-    def mouse(self):
-        driver.get('https://demo.actitime.com/login.do')
-
-mH = mouseHover()
-mH.mouse()
-
+login = loginLRES()
+login.login()
